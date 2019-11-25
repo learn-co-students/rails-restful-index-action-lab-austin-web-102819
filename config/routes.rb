a9dc0832afb08ab97659224d196fa4055c4c49b4
faithe1937 <faithe1937@gmail.com>
2019-11-24 18:55:19 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'students', to: 'students#index'
+get 'students/:id', to: 'students#show'
 
 end
+
+#route to view-has an indexpage
+#multiple students are shown
+#student can be created
+#has a to_s instance method
